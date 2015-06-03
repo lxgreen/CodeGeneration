54ced27757683386a72da6ebaf7ad4ed3148dc18
alex green <alxgreen@gmail.com>
2015-06-04 01:50:36 +0300

    Roslyn-based code generator project added

---
+++ RoslynCodeGenerator/ClassGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynCodeGenerator
+{
+    public class ClassGenerator : CodeGenerator
+    {
+        private SyntaxNode _root;
+
+        public ClassGenerator(string template)
+            : base(template)
+        {
+            _root = SyntaxTree.GetRoot();
+        }
+
+        public string ClassName
+        {
+            get
+            {
+                var name = string.Empty;
+                var classDeclaration = _root.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+                if (classDeclaration != null)
+                {
+                    name = classDeclaration.Identifier.Text;
+                }
+
+                return name;
+            }
+            //set
+            //{
+            //    var classDeclaration = _root.DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+            //    if (classDeclaration != null)
+            //    {
+            //        SyntaxToken t = new SyntaxToken();
+
+            //        //var newDeclaration = classDeclaration.W
+            //    }
+            //}
+        }
+    }
+}
---
+++ RoslynCodeGenerator/ClassNameRewriter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynCodeGenerator
+{
+    public class ClassNameRewriter : CSharpSyntaxRewriter
+    {
+        private SemanticModel _semanticModel;
+        private string _name;
+
+        public ClassNameRewriter(string name, SemanticModel semanticModel)
+        {
+            _semanticModel = semanticModel;
+            _name = name;
+        }
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            var newName = SyntaxFactory.Identifier(node.Identifier.LeadingTrivia, _name, node.Identifier.TrailingTrivia);
+            var newDeclaration = node.Update(node.AttributeLists, node.Modifiers, node.Keyword, newName, node.TypeParameterList, node.BaseList, node.ConstraintClauses, node.OpenBraceToken, node.Members, node.CloseBraceToken, node.SemicolonToken);
+            return newDeclaration;
+        }
+    }
+}
